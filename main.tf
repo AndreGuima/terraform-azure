ec475a46df395e3f29c3cce568e13aadaa97fc9d
AndreGuima <andrepaivaguimaraes@gmail.com>
2023-08-14 11:57:44 -0300

    add ansible tasks

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
   size                  = "Standard_DS1_v2"
-  custom_data           = filebase64("deployWebApp.sh")
+  #custom_data           = filebase64("deployWebApp.sh")
 
   os_disk {
     name                 = "myOsDisk"
